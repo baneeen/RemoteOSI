ea7648f55ea8b8bf8a127b82b077fde5f4f4e3e5
Weijun <Weijun@163.118.237.239>
2013-11-20 16:55:26 -0500

    Test 1
    Signed-off-by: Weijun <Weijun@163.118.237.239>

--- OSI/src/Main.java
+++ OSI/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcom to OSI Project!");
 		System.out.println("Test 1");
-
+		System.out.println("Hello");
 	}
 
 }
